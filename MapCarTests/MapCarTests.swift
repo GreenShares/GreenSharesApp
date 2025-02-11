9a62336724f499e4134d0adb7e111e2455ba3e00
Lethienbao2004 <markle1903@icloud.com>
2025-02-11 13:18:34 -0800

    Initial Commit

---
+++ MapCarTests/MapCarTests.swift
@@ -0,0 +1,16 @@
+//
+//  MapCarTests.swift
+//  MapCarTests
+//
+//  Created by Mark Le on 2/11/25.
+//
+
+import Testing
+
+struct MapCarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
